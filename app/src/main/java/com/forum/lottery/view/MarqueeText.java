26f1bf18bb45a152760751553b931a18826169fb
admin_h <799652526@qq.com>
2017-05-23 00:36:30 +0800

    ..

---
+++ app/src/main/java/com/forum/lottery/view/MarqueeText.java
@@ -0,0 +1,25 @@
+package com.forum.lottery.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+/**
+ * Created by Administrator on 2016/3/17.
+ * 实现跑马灯效果的TextView
+ */
+public class MarqueeText extends android.support.v7.widget.AppCompatTextView {
+    public MarqueeText(Context context) {
+        super(context);
+    }
+    public MarqueeText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public MarqueeText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    //返回textview是否处在选中的状态
+    //而只有选中的textview才能够实现跑马灯效果
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
