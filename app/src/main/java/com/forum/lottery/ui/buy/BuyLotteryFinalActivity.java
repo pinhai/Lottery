31a909717778202fa1c4179ebc6c21c249efeaf5
xionghaiping <799652526@qq.com>
2017-05-25 17:58:34 +0800

    ..

---
+++ app/src/main/java/com/forum/lottery/ui/buy/BuyLotteryFinalActivity.java
@@ -0,0 +1,30 @@
+package com.forum.lottery.ui.buy;
+
+import android.os.Bundle;
+
+import com.forum.lottery.R;
+import com.forum.lottery.ui.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/5/25 0025.
+ */
+
+public class BuyLotteryFinalActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_buy_lottery_final);
+
+    }
+
+    @Override
+    protected void initView(){
+
+    }
+
+    @Override
+    protected void initData(){
+
+    }
+}
